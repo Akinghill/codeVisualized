c53694654e34031d96556bad0286d4cad69ce7ce
akinghill <austinkinghill@gmail.com>
2020-11-14 16:30:40 -0700

    lighthouse boosts

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = (params) => (
   <header className='header'>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a style={{height:'75px'}} className="navbar-brand" href=".">
+      <Link style={{height:'75px'}} className="navbar-brand" to="./" aria-label="Home Logo">
         <img style={{height:'100%'}} src="./assets/logo.svg" alt=""/>
-      </a>
+      </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -13,12 +14,12 @@ export const Header = (params) => (
       <div className="collapse navbar-collapse" id="navbarColor02">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href=".">Home
+            <Link className="nav-link" to="./">Home
           <span className="sr-only">(current)</span>
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="./blog">Blog</a>
+            <Link className="nav-link" to="./blog">Blog</Link>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#about">About</a>
--- src/pages/blog/blog-page.component.jsx
+++ src/pages/blog/blog-page.component.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 
+import Navbar from '../../components/navbar/navbar.component'
+import { Card } from '../../components/card/card.component'
+
+import './blog-page.styles.scss'
+
 export default function BlogPage() {
   return (
     <div>
-      Blog Page
+      <Navbar />
+      <section className="section ph-1r">
+        <h2 className="posts-title">Posts</h2>
+        <div className="blog-posts-list">
+
+          <div className="post">
+            <div className="img-container">
+              <img className="post-img" src="./assets/blogpost1.jpg" alt="post" />
+            </div>
+
+            <div className="post-content-preview">
+              <h3>Post title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolor fuga nihil aliquam repellat quas, porro molestias, quisquam tenetur placeat unde libero magni quam quibusdam obcaecati architecto optio necessitatibus quasi! Praesentium harum, iusto commodi esse hict.</p>
+            </div>
+          </div>
+          <div className="post">
+            <div className="img-container">
+              <img className="post-img" src="./assets/blogpost1.jpg" alt="post" />
+            </div>
+
+            <div className="post-content-preview">
+              <h3>Post title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolor fuga nihil aliquam repellat quas, porro molestias, quisquam tenetur placeat unde libero magni quam quibusdam obcaecati architecto optio necessitatibus quasi! Praesentium harum, iusto commodi esse hict.</p>
+            </div>
+          </div>
+
+
+        </div>
+      </section>
     </div>
   )
 }
